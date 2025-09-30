c1a4a677fbfe855cafd15f55493a3e315b410041
Baffetry <Aleksandrda71@gmail.com>
2025-09-30 20:32:23 +0300

    Created Teacher UI with some functions

--- TeacherGraphApplication/TeacherGraphApplication/MainWindow.xaml.cs
+++ TeacherGraphApplication/TeacherGraphApplication/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TeacherGraphApplication
 {
@@ -20,5 +13,46 @@ namespace TeacherGraphApplication
         {
             InitializeComponent();
         }
+
+        #region ButtonProperties
+        private void ButtonProperties_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ButtonProperties.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4dd49e"));
+        }
+
+        private void ButtonProperties_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ButtonProperties.Background = Brushes.White;
+        }
+        #endregion
+
+        #region ButtonResults
+        private void ButtonResults_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ButtonResults.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4dd49e"));
+        }
+
+        private void ButtonResults_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ButtonResults.Background = Brushes.White;
+        }
+        #endregion
+
+        #region ButtonExit
+        private void ButtonExit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ButtonExit.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#db5174"));
+        }
+
+        private void ButtonExit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ButtonExit.Background = Brushes.White;
+        }
+        
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
     }
 }
