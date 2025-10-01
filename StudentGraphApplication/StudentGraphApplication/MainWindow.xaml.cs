aec9afc30d26171c4930a164a9b455c405709b5b
k1nd0r <matveyv859@gmail.com>
2025-10-01 16:16:50 +0300

    Add interface for Students

--- StudentGraphApplication/StudentGraphApplication/MainWindow.xaml.cs
+++ StudentGraphApplication/StudentGraphApplication/MainWindow.xaml.cs
@@ -1,24 +1,84 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace StudentGraphApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private int timeInSeconds = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
         }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            TaskPanel.Visibility = Visibility.Collapsed;
+            MenuPanel.Visibility = Visibility.Visible;
+            timer.Stop();
+            timeInSeconds = 0;
+        }
+        #region SaveButton
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveButton.Background = new SolidColorBrush(Colors.White);
+        }
+        #endregion
+        #region StartButton
+        private void StartButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StartButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4dd49e"));
+        }
+
+        private void StartButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StartButton.Background = Brushes.White;
+        }
+        #endregion
+
+        #region StartButton
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPanel.Visibility = Visibility.Collapsed;
+            TaskPanel.Visibility = Visibility.Visible;
+            timer.Start();
+        }
+        private void FileButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            FileButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4dd49e"));
+        }
+
+        private void FileButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            FileButton.Background = Brushes.White;
+        }
+        #endregion
+
+        #region ExitButton
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void ExitButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            ExitButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#db5174"));
+        }
+        private void ExitButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            ExitButton.Background = Brushes.White;
+        }
+        #endregion
     }
 }
